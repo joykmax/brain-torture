254486cca0fb820135be1f0ccf393021d22d1f25
joykmax <joykillermax@gmail.com>
2022-08-29 18:09:27 +0200

    tiny fix

--- PieChart/src/main/java/com/joykmax/PieChart.java
+++ PieChart/src/main/java/com/joykmax/PieChart.java
@@ -10,19 +10,19 @@ import java.io.IOException;
 public class PieChart {
 
     public static BufferedImage createChart(ChartData[] chartData){
-        Integer width = 200;
-        Integer height = 200;
+        Integer width = 400;
+        Integer height = 400;
         Integer startChartX = 0;
-        Integer startChartY = 10;
-        Integer startTextX = 10;
+        Integer startChartY = 20;
+        Integer startTextX = 20;
         Integer startTextY = height + startChartY * 2;
-        Integer lineHeight = 24;
+        Integer lineHeight = 48;
         Integer fullwidth = width;
         Integer fullheight = startTextY + chartData.length * lineHeight;
 
         BufferedImage bImage = new BufferedImage(fullwidth,fullheight,BufferedImage.TYPE_INT_RGB);
         Graphics2D g2d = bImage.createGraphics();
-        Font font = new Font("Verdana", Font.PLAIN, 11);
+        Font font = new Font("Verdana", Font.BOLD, 16);
         g2d.setFont(font);
         RenderingHints rh = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         rh.put(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
@@ -52,11 +52,10 @@ public class PieChart {
         for (ChartData item:chartData) {
             g2d.setColor(colors[index]);
             nextAngle = (int) (item.getPercentVal() * koef);
-            System.out.println(currentAngle + ":" + nextAngle);
             g2d.fillArc(startChartX, startChartY, startChartX+width, startChartY+height, currentAngle, nextAngle);
             currentAngle += nextAngle;
 
-            g2d.fillRect(startTextX,startTextY+lineHeight*index,20,20);
+            g2d.fillRect(startTextX,startTextY+lineHeight*index,40,40);
 
             g2d.setColor(new Color(0, 0, 0));
             g2d.drawString(item.getText(), startTextX + lineHeight, (int)(startTextY+lineHeight*index+lineHeight/2));
